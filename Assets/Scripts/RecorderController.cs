f7ffa51e3ba1417a21d4319e67c97c788b2db72c
donghokang <east0822@gmail.com>
2019-10-04 18:42:26 +0200

    ffmpeg video recording wip

--- Assets/Scripts/RecorderController.cs
+++ Assets/Scripts/RecorderController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 
@@ -16,9 +17,9 @@ namespace raisimUnity
         private int _width = 1920;
         private int _height = 1080;
 
-        private String _outFile = "test.mp4";
-        private Process _ffmpegProc;
+        private Process _ffmpegProc = null;
 
+        private String _outFile = "/tmp/test.mp4";
         private bool _isRecording = false;
         public bool IsRecording
         {
@@ -28,36 +29,49 @@ namespace raisimUnity
         float FrameTime {
             get { return _startTime + (_frameCount - 0.5f) / _frameRate; }
         }
-        
+
         public void StartRecording()
         {
-            // props
-            _isRecording = true;
-            _startTime = Time.time;
-            _frameCount = 0;
-            
             // render texture depth check
             _rt = new RenderTexture(1920, 1080, 16, RenderTextureFormat.ARGB32);
             _rt.Create();
             gameObject.GetComponent<Camera>().targetTexture = _rt;
             
             // ffmpeg 
+            // TODO what about Mac and Windows?
             ProcessStartInfo psi = new ProcessStartInfo();
             psi.FileName = "/bin/sh";
             psi.UseShellExecute = false;
             psi.CreateNoWindow = true;
             psi.RedirectStandardInput = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
             psi.Arguments = 
-                "ffmpeg -r " + _frameRate.ToString() + 
-                " -f rawvideo -pix_fmt rgb24 -s " + _width.ToString() + "x" + _height.ToString() +
-                " -i - -threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 " + _outFile;
+                "-c \"" +
+                "ffmpeg -r " + _frameRate.ToString() + " -f rawvideo -pix_fmt rgb24 -s " + _width.ToString() + "x" + _height.ToString() +
+                " -i - -threads 0 -preset fast -y -pix_fmt yuv420p -crf 21 " + _outFile + "\"";
             _ffmpegProc = Process.Start(psi);
+            
+            if (_ffmpegProc != null)
+            {
+                // TODO exception
+//                print(_ffmpegProc.StandardError.ReadToEnd());
+                
+                _isRecording = true;
+                _startTime = Time.time;
+                _frameCount = 0;
+            }
+            else
+            {
+                // TODO exception
+            }
         }
 
         public void FinishRecording()
         {
             if (_isRecording)
             {
+                _ffmpegProc.StandardInput.BaseStream.Close();
                 _ffmpegProc.Close();
             }
             
@@ -76,9 +90,9 @@ namespace raisimUnity
             texture.Apply();
             RenderTexture.active = null;
 
-            var ffmpegIn = _ffmpegProc.StandardInput.BaseStream;
-            ffmpegIn.Write(texture.GetRawTextureData(), 0, 0);
-            ffmpegIn.Flush();
+            // write to ffmpeg
+            _ffmpegProc.StandardInput.BaseStream.Write(texture.GetRawTextureData(), 0, 0);
+            _ffmpegProc.StandardInput.BaseStream.Flush();
         }
 
         void FixedUpdate()
